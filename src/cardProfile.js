4c5b1c1c4d2bc2153e7b5efd6f8672862fd30fa8
stefnotadevyet <srjack91@gmail.com>
2021-08-04 13:35:49 -0400

    added testData file with formatted res data for visual

---
+++ src/cardProfile.js
@@ -0,0 +1,14 @@
+import React, { component } from 'react'
+
+class cardProfile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false
+    }
+  }
+
+  render () {
+
+  }
+}
