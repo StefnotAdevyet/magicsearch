78bff3c93a49d9533b0f84a0dfe97923250f78fd
stefnotadevyet <srjack91@gmail.com>
2021-09-09 23:59:33 -0400

    changed App to class

--- src/App.js
+++ src/App.js
@@ -17,29 +17,46 @@ the first item in the response data when the app start up
 */
 
 
-function App () {
+class App extends React.Component {
 
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [cardData, setCardData] = useState([]);
-  const [currentPage, setCurrentPage] = useState(endpoint);
-  const [pageIndex, setPageIndex] = useState(1)
-  const [nextPage, setNextPage] = useState();
-  const [previousPage, setPreviousPage] = useState();
-  const [searchTerm, setSearchTerm] = useState('')
+  constructor() {
+    super();
+    this.state = {
+      cards: [],
+      searchTerm: [],
+      totalResults: 0,
+      currentPage: 1,
+      currentCard: null
+    }
+  }
+
+  // const [error, setError] = useState(null);
+  // const [isLoaded, setIsLoaded] = useState(false);
+  // const [cardData, setCardData] = useState([]);
+  // const [currentPage, setCurrentPage] = useState(endpoint);
+  // const [pageIndex, setPageIndex] = useState(1)
+  // const [nextPage, setNextPage] = useState('');
+  // const [previousPage, setPreviousPage] = useState('');
+  // const [searchTerm, setSearchTerm] = useState('')
+
+  var parse = require('parse-link-header')
 
 
 
   useEffect(() => {
-   const fetchCards = async () => {
-     setIsLoaded(true);
-     const res = await axios.get(currentPage);
-     setCardData(res.data.cards);
-     setIsLoaded(false);
+    const fetchCards = async () => {
+      setIsLoaded(true);
+      const res = await axios.get(currentPage);
+      let next = parse(res.headers.link).next.url;
+      let last = parse(res.headers.link).last.url;
+      setPreviousPage(last);
+      setNextPage(next);
+      setCardData(res.data.cards);
+      setIsLoaded(false);
     }
 
     fetchCards();
-  }, []);
+  }, [currentPage]);
 
   function goToNextPage () {
     setCurrentPage(nextPage);
@@ -50,6 +67,10 @@ function App () {
     setCurrentPage(previousPage);
   }
 
+
+  console.log(cardData)
+  console.log(`next page: ${typeof(nextPage)}`)
+  console.log(`last page: ${previousPage}`)
 
 
   return (
