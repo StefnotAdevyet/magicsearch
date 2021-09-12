4932f0a1f0b1356ba9f1a405ae2985ff32805383
stefnotadevyet <srjack91@gmail.com>
2021-09-11 22:29:31 -0400

    added comment

--- src/components/cardProfile.js
+++ src/components/cardProfile.js
@@ -5,12 +5,12 @@ const CardProfile = (props) => {
 
 
 console.log(props);
+//need to make card clickable to view card data
 
     return (
         <div className="container">
             <div className="row">
                 <div className="col s12">
-                    <div className="name">{props.card.name}</div>
                     <img src={props.card.imageUrl} />
                 </div>
             </div>
