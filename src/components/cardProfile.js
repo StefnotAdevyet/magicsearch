8488f97ba1b2636e81d9ab46c70b7d3c32a33b57
stefnotadevyet <srjack91@gmail.com>
2021-09-14 21:50:30 -0400

    added image not found placeholder

--- src/components/cardProfile.js
+++ src/components/cardProfile.js
@@ -8,10 +8,13 @@ console.log(props);
 //need to make card clickable to view card data
 
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col s12">
-                    <img src={props.card.imageUrl} />
+        <div className="col s12 m6 l3">
+            <div className="card">
+                <div className="card-image waves-effect waves-block waves-light">
+                   {
+                       props.card.imageUrl === null ? <img src="https://dredgit.com/wp-content/uploads/2013/11/dummy-image-square.jpg" alt="card image" style={{ width: "100%", height: 150 }}/> :
+                       <img src={props.card.imageUrl} alt="card image" style={{ width: "100%" }}/>
+                   }
                 </div>
             </div>
         </div>
